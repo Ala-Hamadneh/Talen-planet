0abad18ebcf0f637b4a6bf2eb2ba68b0f66dad65
Alaa <alahamadneh35@gmail.com>
2025-04-28 21:43:54 +0300

    Updated the UserListView to return users ordered by id

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -39,7 +39,7 @@ class LoginView(TokenObtainPairView):
         return response
 
 class UserListView(generics.ListAPIView):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
 
