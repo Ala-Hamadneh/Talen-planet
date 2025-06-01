7db569273d4546ed880247e44e792e9b42a34f9a
Alaa <alahamadneh35@gmail.com>
2025-06-01 19:13:29 +0300

    updated the review serializers file to send the correct response to the frontend

--- apps/reviews/serializers.py
+++ apps/reviews/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
+    reviewer = serializers.CharField(source='reviewer.username', read_only=True)
+    reviewer_image = serializers.ImageField(source='reviewer.profile_picture', read_only=True)
     class Meta:
         model = Review
         fields = '__all__'
-        read_only_fields = ('reviewer', 'seller', 'gig', 'order', 'created_at', 'is_active')
+        read_only_fields = ('reviewer', 'reviewer_image', 'seller', 'gig', 'order', 'created_at', 'is_active')
